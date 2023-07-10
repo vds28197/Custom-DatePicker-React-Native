81deb6f9b49e20f8a4054c4ea4d718f4badd6ec7
vds28197 <vds.s28197@gmail.com>
2023-07-10 23:59:33 +0530

    Month name with previous and change next month button, Print week days

--- components/DatePickerModal.js
+++ components/DatePickerModal.js
@@ -1,16 +1,63 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
 import {Modal} from 'native-base';
+import MIcon from 'react-native-vector-icons/MaterialIcons';
+
+const monthsName = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const DatePickerModal = ({showModal, onClose}) => {
+  const [getCurrentMonth, setCurrentMonth] = useState(new Date().getMonth());
+  const [month, setMonth] = useState(getCurrentMonth);
+
+  const [getCurrentDate, setCurrentDate] = useState(new Date().getDate());
+  const [getUserSelectedDate, setUserSelectedDate] = useState(getCurrentDate);
+
   return (
     <Modal isOpen={showModal} onClose={onClose}>
       <Modal.Content style={styles.modalContainer}>
         <Modal.Header style={styles.headerWrapper}>
           <Text style={styles.headerContent}>Date - Picker</Text>
         </Modal.Header>
-        <Modal.Body>
-          <Text>Hello1</Text>
+        <Modal.Body style={styles.bodyContainer}>
+          <View style={styles.bodyHeaderContent}>
+            <TouchableOpacity
+              onPress={() => setMonth(month - 1)}
+              disabled={month == getCurrentMonth}>
+              <MIcon name="keyboard-arrow-left" size={30} color={'#FFFFFF'} />
+            </TouchableOpacity>
+            <Text style={styles.monthNameTextStyle}>{monthsName[month]}</Text>
+            <TouchableOpacity
+              onPress={() => setMonth(month + 1)}
+              disabled={month == 11}>
+              <MIcon name="keyboard-arrow-right" size={30} color={'#FFFFFF'} />
+            </TouchableOpacity>
+          </View>
+          <View style={{marginTop: 10}}>
+            <View style={{flexDirection: 'row'}}>
+              {weekDays.map(day => {
+                return (
+                  <View key={day} style={styles.weekLabel}>
+                    <Text style={{color: '#ffffff'}}>{day}</Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
         </Modal.Body>
         <Modal.Footer>
           <Text>Hello1</Text>
@@ -34,5 +81,25 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  bodyContainer: {
+    backgroundColor: '#191919',
+  },
+  bodyHeaderContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  monthNameTextStyle: {
+    letterSpacing: 1,
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  weekLabel: {
+    flex: 1,
+    alignItems: 'center',
+    height: 40,
+    justifyContent: 'center',
   },
 });
